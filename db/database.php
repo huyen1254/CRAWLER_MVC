16005c2015c88afbc4a77c3278bcb1a0b212fdb8
huyen125 <53432310+huyen1254@users.noreply.github.com>
2019-11-14 22:14:40 +0700

    oop

--- db/database.php
+++ db/database.php
@@ -9,7 +9,7 @@
             include __DIR__ . '/../config/config.php';
             $this->conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME) or die("Error connect");
         }
-        public function ($table_name, $db_fields_name, $values_arr){
+        public function addPage($table_name, $db_fields_name, $values_arr){
             $sql = "INSERT INTO `$table_name` ($db_fields_name) VALUES ('$values_arr')";
             mysqli_query($this->conn, $sql) or die('Error query!');
         }
